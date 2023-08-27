8602b0eb52a9883f73ed4274c3e8d98fffe6c91f
VanshikaKhajanchii <vanshika.khajanchi@outlook.com>
2023-08-27 19:39:50 +0530

    Add project files.

---
+++ Keypad_Code_Translator/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Keypad_Code_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodeBox = new System.Windows.Forms.RichTextBox();
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.toText = new System.Windows.Forms.Button();
+            this.toCode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.msgLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CodeBox
+            // 
+            this.CodeBox.Location = new System.Drawing.Point(43, 114);
+            this.CodeBox.Name = "CodeBox";
+            this.CodeBox.Size = new System.Drawing.Size(302, 150);
+            this.CodeBox.TabIndex = 0;
+            this.CodeBox.Text = "";
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(433, 114);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(316, 150);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.Text = "";
+            // 
+            // toText
+            // 
+            this.toText.Location = new System.Drawing.Point(93, 293);
+            this.toText.Name = "toText";
+            this.toText.Size = new System.Drawing.Size(194, 59);
+            this.toText.TabIndex = 2;
+            this.toText.Text = "Convert To Text";
+            this.toText.UseVisualStyleBackColor = true;
+            this.toText.Click += new System.EventHandler(this.ConvertToText);
+            // 
+            // toCode
+            // 
+            this.toCode.Location = new System.Drawing.Point(487, 293);
+            this.toCode.Name = "toCode";
+            this.toCode.Size = new System.Drawing.Size(194, 59);
+            this.toCode.TabIndex = 3;
+            this.toCode.Text = "Convert To Code";
+            this.toCode.UseVisualStyleBackColor = true;
+            this.toCode.Click += new System.EventHandler(this.ConvertToCode);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Code Here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Text Here";
+            // 
+            // msgLabel
+            // 
+            this.msgLabel.AutoSize = true;
+            this.msgLabel.Location = new System.Drawing.Point(39, 26);
+            this.msgLabel.Name = "msgLabel";
+            this.msgLabel.Size = new System.Drawing.Size(0, 20);
+            this.msgLabel.TabIndex = 6;
+            this.msgLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.msgLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toCode);
+            this.Controls.Add(this.toText);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.CodeBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox CodeBox;
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button toText;
+        private System.Windows.Forms.Button toCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label msgLabel;
+    }
+}
+
---
+++ Keypad_Code_Translator/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keypad_Code_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void ConvertToText(object sender, EventArgs e)
+        {
+            var code = CodeBox.Text;
+            string[] allcodewords = code.Trim().Split(' ');
+            string TextWords = string.Empty;
+            string allTextWords= string.Empty;
+
+            foreach (string item in allcodewords)
+            {
+                char[] signlecodeWord = item.ToArray();//{2,2,3,3,4,5}
+                //tostore code for each alphabet  22,33,4,5
+                List<string> charcodelist = new List<string>();
+                string character = string.Empty;
+                int lastindex = 0;
+                int count = 0;
+                for (int i =0;i<signlecodeWord.Length;i++)
+                {
+                    if(i!=signlecodeWord.Length-1)
+                    {
+                        if (signlecodeWord[i] == signlecodeWord[i + 1])
+                        {
+                            count++;
+                            //lastindex = i+1;
+                            continue;
+                        }
+                        else
+                        {
+                            lastindex= i;
+                            character = string.Empty;
+
+                            for (int j = 0; j <= count; j++)
+                            {
+                                character += signlecodeWord[lastindex];
+                                --lastindex;
+                            }
+                            count = 0;
+                            lastindex = 0;
+                            charcodelist.Add(character);
+                        }
+                    }                   
+                    else
+                    {
+                        lastindex = i;
+                        character = string.Empty;
+                        for (int j=0;j<=count;j++)
+                        {
+                            character += signlecodeWord[lastindex];
+                            --lastindex;
+                        }
+                        charcodelist.Add(character);
+                    }
+                }
+                TextWords = string.Empty;
+                foreach(var letter in charcodelist)
+                {
+                    if(letter == ".")
+                    {
+                        continue;
+                    }
+                    var text = (Code)Int32.Parse(letter.ToString());
+                    if (!Regex.IsMatch(text.ToString(), "[a-z]"))
+                    {
+                        msgLabel.Text = "Please check the input!";
+                        break;
+
+                    }
+                    TextWords += text;
+                }
+                allTextWords += TextWords + " ";
+            }
+            TextBox.Text = allTextWords;
+        }
+
+        private void ConvertToCode(object sender, EventArgs e)
+        {
+            var code = TextBox.Text;
+            string[] allcodewords = code.ToLower().Trim().Split(' ');
+            string TextWords = string.Empty;
+            string allTextWords = string.Empty;
+            
+
+            foreach (string item in allcodewords)
+            {
+                char[] signlecodeWord = item.ToArray();//{2,2,3,3,4,5}
+                //tostore code for each alphabet  22,33,4,5
+                TextWords = string.Empty;
+                foreach (var letter in signlecodeWord)
+                {
+                    Code charac;
+                    
+                    Enum.TryParse(letter.ToString(), out charac);
+                    var text = (uint)charac;
+                    var lastChar = TextWords != string.Empty?TextWords.Substring(TextWords.Length-1)
+                        :string.Empty;
+                    if (lastChar == text.ToString().Substring(0,1))
+                        TextWords += ".";
+                    TextWords += text;
+                }
+                allTextWords += TextWords + " ";  
+            }
+            CodeBox.Text = allTextWords;
+
+        }
+
+        private void OnClick_ConvertToText(object sender, EventArgs e)
+        {
+            var code = CodeBox.Text;
+
+        }
+        private void OnClick_ConvertToCode(object sender, EventArgs e)
+        {
+            var code = CodeBox.Text;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
